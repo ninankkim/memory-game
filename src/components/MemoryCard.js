70b8bec55ceb031e03107668b37e06436899c56f
Nina Kim <ninakim0504@hotmail.com>
2020-10-23 17:54:56 -0400

    exercise1

---
+++ src/components/MemoryCard.js
@@ -0,0 +1,12 @@
+import './MemoryCard.css';
+import React from 'react';
+
+export default class MemoryCard extends React.Component {
+    render() {
+        return(
+            <div className="MemoryCard">
+                <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png"/>
+            </div>
+        )
+    }
+}
