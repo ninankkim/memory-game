e998d479521f870e1925f5e2842652a822729287
Nina Kim <ninakim0504@hotmail.com>
2020-10-26 14:45:19 -0400

    files

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,11 +1,25 @@
+import React from 'react'
 import './MemoryCard.css';
-import React from 'react';
-
 export default class MemoryCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isFlipped: false};
+    }
+    clickHandler = () => {
+        this.setState({isFlipped : !this.state.isFlipped})
+    };
     render() {
-        return(
-            <div className="MemoryCard">
-                <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png"/>
+        const innerClass = this.state.isFlipped ? 'MemoryCard__inner flipped' : 'MemoryCard__inner'
+        return (
+            <div className="MemoryCard" onClick={this.clickHandler}>
+                <div className={innerClass}>
+                    <div className="MemoryCard__back">
+                        <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png"></img>
+                    </div>
+                    <div className="MemoryCard__front">
+                        ∆
+                    </div>
+                </div>
             </div>
         )
     }
